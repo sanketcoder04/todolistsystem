951b5d4d36ff3b863d0e72d1cbfe4b297bfd4ebd
Sanket Ganguly <sanketganguly53@gmail.com>
2024-07-21 02:33:44 +0530

    Update to-do-list.sql

--- to-do-list.sql
+++ to-do-list.sql
@@ -4,7 +4,7 @@ SHOW TABLES;
 
 SET SQL_SAFE_UPDATES = 0;
 
-INSERT INTO ajay (tasks) VALUES ("Sleeping for two hours");
+INSERT INTO ajay (tasks) VALUES ("Sleeping for two hours");    -- for testing purpose
 SELECT * FROM ajay;
 
 SELECT * FROM sanket;
